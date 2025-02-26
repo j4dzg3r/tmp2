e7c832cae905c1682d36c362005d0b8c9a8b4445
Ivan Golyshev <golyshev.nomer@gmail.com>
2025-02-26 23:56:49 +0300

    ci: connect env_file to backend

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -80,8 +80,16 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('PGBOUNCER_HOST'),
+        'PORT': os.getenv('PGBOUNCER_PORT'),
+        'CONN_MAX_AGE': 60,
+        'OPTIONS': {
+            'connect_timeout': 10
+        },
     }
 }
 
